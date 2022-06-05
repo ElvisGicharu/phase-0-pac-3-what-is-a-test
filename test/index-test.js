ed41f5670b9a7a6d6c73342ecb0892fad1f95e77
elvis <gicharuelvis@gmail.com>
2022-06-05 23:56:43 +0300

    commit tree 974aa7fc48978291947ec5ec8594a35578caa24b

--- test/index-test.js
+++ test/index-test.js
@@ -7,6 +7,7 @@ describe('what-is-a-test', () => {
       expect(name).toEqual('Susan')
     })
   })
+  
 
 
   describe('Height', () => {
@@ -23,3 +24,22 @@ describe('what-is-a-test', () => {
   })
 })
 */
+describe("Name", () => {
+  it('returns "Susan"', () => {
+    expect(name).toEqual("Susan");
+  });
+});
+
+describe("Height", () => {
+  it("is less than 40", () => {
+    expect(height).toBeLessThan(40);
+  });
+});
+
+describe("Message", () => {
+  it("gives the name and height", () => {
+    expect(message).toInclude(name);
+    expect(message).toInclude(height);
+  });
+});
+
